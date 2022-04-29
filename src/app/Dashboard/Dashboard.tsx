2bc19c5bbda8a47f665554322d8fb7efb303f2be
Navya923 <navyaramesh333@gmail.com>
2022-04-29 12:21:52 +0530

    todopattern

--- src/app/Dashboard/Dashboard.tsx
+++ src/app/Dashboard/Dashboard.tsx
@@ -4,6 +4,33 @@ import { PageSection, Title } from '@patternfly/react-core';
 const Dashboard: React.FunctionComponent = () => (
   <PageSection>
     <Title headingLevel="h1" size="lg">Dashboard Page Title!</Title>
+    const [fullname, setFullname] = useState([]);
+
+const onSubmit = (e) => {
+  
+  console.log(fullname);
+  
+}
+return (
+  <div className="App">
+<h1>TODO APP </h1>
+
+
+<form onSubmit={onsubmit}>
+<input type="text"
+value={fullname}
+placeholder="Enter your name"
+onChange= {setFullname}/>
+
+<div>
+<button type="submit" onClick={onSubmit}>submit</button>
+</div>
+
+</form>
+
+  </div>
+);
+}
   </PageSection>
 )
 
